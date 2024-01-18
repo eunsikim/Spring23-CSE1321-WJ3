25be018c617ac80a8ad420327f175c57df34e804
eunsikim <kimeunsik229@gmail.com>
2024-01-18 14:48:24 -0500

    Math.sqrt() example

---
+++ Week_02/ExampleH.java
@@ -0,0 +1,9 @@
+public class ExampleH {
+    public static void main(String[] args) {
+        float r1 = 5;
+        float r2 = 6;
+        
+        //Example on how to use Math.sqrt()
+        System.out.println(Math.sqrt(((r2 - r1) * (r2 - r1))));
+    }
+}
