02a0b3d3019de29098272f37a8c050e93935ae2f
eunsikim <kimeunsik229@gmail.com>
2024-02-15 15:11:04 -0500

    RNG range manipulation

--- Week_06/rng.java
+++ Week_06/rng.java
@@ -4,9 +4,15 @@ public class rng {
     public static void main(String[] args) {
         Random rand = new Random();
 
+        
         for(int i = 0; i < 10; i++){
-            int randomNumber = rand.nextInt(123);
-            System.out.println(randomNumber);
+            // Generate a number [0, 9] or [0, 10)
+            int randomNumber1 = rand.nextInt(10); 
+            // Generate a number from [0, 5], then shift the values by +5 so: [5, 10]
+            int randomNumber2 = rand.nextInt(6) + 5; 
+            System.out.println("[0, 9]: " + randomNumber1);
+            System.out.println("[5, 10]: " + randomNumber2);
+            System.out.println();
         }
     }
 }
