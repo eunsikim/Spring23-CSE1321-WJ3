378001a360e47f826e892f30f2c9123fd9a5ab5c
eunsikim <kimeunsik229@gmail.com>
2024-02-22 13:04:04 -0500

    Week 7

---
+++ Week_07/Appendix.java
@@ -0,0 +1,11 @@
+public class Appendix {
+    public static void main(String[] args) throws InterruptedException {
+        String word = "Hello World";
+
+        System.out.print(word);
+
+        // Deletes the last characters
+        System.out.print("\b");
+        Thread.sleep (500);
+    }
+}
---
+++ Week_07/README.md
@@ -0,0 +1,4 @@
+# Contents
+
+Link to Miro Board:
+[Click here](https://miro.com/app/board/uXjVNp2NmNE=/?share_link_id=805010347641)
